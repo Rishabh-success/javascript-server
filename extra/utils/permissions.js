7c047db9af50717279a0f073d229d7d37d342167
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2020-10-21 17:50:28 +0530

    feat:Create file to validate email

---
+++ extra/utils/permissions.js
@@ -0,0 +1,19 @@
+function createequilateral(n){
+    let s = "";
+    let j = n;
+    for(let i = 1; i<=n; i++){
+        s=" ".repeat(j);
+        console.log(s,"* ".repeat(i));
+        s="";
+        j--;
+    }
+    for(let i = n; i>=1; i--){
+        s=" ".repeat(j);
+        console.log(s," *".repeat(i));
+        s="";
+        j++;
+    }
+}
+
+n = process.argv[2]
+createequilateral(n); 
