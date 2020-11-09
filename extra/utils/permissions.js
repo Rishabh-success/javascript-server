ffb9b33bd2e1e23197fa9de6e93afcf8cd8f8f19
Rishabh-success <72821460+Rishabh-success@users.noreply.github.com>
2020-11-09 17:19:18 +0530

    Update permissions.js

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,13 +1,9 @@
 import {permissions} from "../constants.js"
-const {getUsers}= permissions;
 export default function hasPermission(moduleName,role,permissionType)
-function hasPermission(moduleName,role,permissionType)
 {
-const {all}=moduleName;
 return all.includes(role) || moduleName[permissionType].includes(role);
 }
 console.log(permissions.getUsers)
-let d = hasPermission(getUsers,"head-trainer","Delete");
-console.log(d);
-//if i have pass value role:head-trainer and permissiontype:read,write,delete all case value is true
-////if i have pass value role:trainee and permissiontype:write O/P false,permissiontype:read O/P true,permissiontype:Delete O/P false,
+let output = hasPermission('getUsers','head-trainer','Delete');
+console.log(output);
+
