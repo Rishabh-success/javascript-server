c9775db584870f2a8f668a630140787f61f68826
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2020-11-10 19:09:46 +0530

    feat:Generic Routes Validations

--- dist/controllers/trainee/routes.js
+++ dist/controllers/trainee/routes.js
@@ -2,11 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const Controller_1 = require("./Controller");
+const validationHandler_1 = require("../../libs/routes/validationHandler");
+const validation_1 = require("./validation");
 const traineeRouter = express_1.Router();
 traineeRouter.route('/')
-    .get(Controller_1.default.get)
-    .post(Controller_1.default.create)
-    .put(Controller_1.default.update)
-    .delete(Controller_1.default.delete);
+    .get(validationHandler_1.default(validation_1.default.get), Controller_1.default.get)
+    .post(validationHandler_1.default(validation_1.default.create), Controller_1.default.create)
+    .put(validationHandler_1.default(validation_1.default.update), Controller_1.default.update)
+    .delete(validationHandler_1.default(validation_1.default.Delete), Controller_1.default.delete);
 exports.default = traineeRouter;
 //# sourceMappingURL=routes.js.map
