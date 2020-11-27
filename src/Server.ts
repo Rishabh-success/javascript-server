c9144e3c0f4c7ac728d33d3e6ea9ad55364198be
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2020-11-27 22:42:54 +0530

    feat:Server updated

--- src/Server.ts
+++ src/Server.ts
@@ -1,27 +1,16 @@
 import * as express from 'express';
-import { notFoundHandler, errorHandler } from './libs/routes';
-import mainRouter from './router';
-import {IConfig}  from './config/IConfig';
-import { config } from './config';
+    import {IConfig}  from './config/IConfig';
+    import { config } from './config';
 class Server {
     private app: express.Express;
     constructor(private config:IConfig) {
         this.app = express();
     }
     bootstrap() {
-
         this.SetupRoutes();
         return this;
     }
     SetupRoutes() {
-        this.app.use('/api', mainRouter);
-        this.app.get('/health-check', (req, res, next) => {
-            res.send('i am ok');
-        });
-
-        this.app.use(notFoundHandler);
-
-        this.app.use(errorHandler);
         const { app } = this;
         app.get('/health-check', (req, res, next) => {
             res.send('i am ok');
@@ -32,8 +21,7 @@ class Server {
         console.log(config);
         const { app, config: { port } } = this;
         app.listen(port)
-            
-            console.log(`App is running on port ${port}`);
     }
+
 }
 export default Server;
