eb5f341ee3d2878f67ff670f52ec2298164a94d8
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2020-12-21 17:50:14 +0530

    feat:Models setup with initial Seed

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
+import validationHandler from '../../libs/routes/validationHandler';
 import UserController from './Controller';
+import config from './validation';
 
 const UserRouter = Router();
 
 UserRouter.route('/')
-   .get (UserController.get)
-   .post (UserController.create)
-   .put (UserController.update)
-   .delete (UserController.delete);
+.get(validationHandler(config.get), UserController.get)
+.post(validationHandler(config.create), UserController.create)
+.put(validationHandler(config.update), UserController.update)
+.delete(validationHandler(config.Delete), UserController.delete);
 
 export default UserRouter;
