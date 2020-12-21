3748358e6ed304885eb9ee238a42b5a3ac55caaa
RISHABH SINGH <rishabh.singh@rishabhsingh.successivetech.com>
2020-12-21 19:09:14 +0530

    feat:Profile Setup

--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -4,13 +4,14 @@ import authMoiddleWare from '../../libs/routes/authMiddleWare';
 import validation from './validation'
 import validationHandler from '../../libs/routes/validationHandler';
 import { getUsers, getDetails } from '../constants';
+import config from './validation';
 
 const UserRouter = Router();
 UserRouter.route('/')
-   .get(UserController.get)
-   .post(UserController.create)
-   .put(UserController.update)
-   .delete(UserController.delete);
+.get(validationHandler(config.get), UserController.get)
+.post(validationHandler(config.create), UserController.create)
+.put(validationHandler(config.update), UserController.update)
+.delete(validationHandler(config.Delete), UserController.delete);
 
 UserRouter.route('/me')
    .get(authMoiddleWare('getUsers', 'all'), UserController.me)
